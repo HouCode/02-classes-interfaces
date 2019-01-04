d6ba2342c4e678038f6637c1add3003126fa08b6
HouCode <it.code@gmx.de>
2019-01-04 16:22:24 +0100

    implement Element and Iterator + add Iterable Interface

--- src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
+++ src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
@@ -1,14 +1,49 @@
 package de.thro.inf.prg3.a02;
 
+import java.util.Iterator;
+
 /**
  * @author Peter Kurfer
  * Created on 10/6/17.
  */
-public class SimpleListImpl implements SimpleList {
+public class SimpleListImpl implements SimpleList, Iterable {
 
+
+    @Override
+    public void add(Object o) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public SimpleList filter(SimpleFilter filter) {
+        return null;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
 
     private static class Element{
         Object item;
         Element next;
+    }
+
+    class SimpleIteratorImpl implements Iterator{
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public Object next() {
+            return null;
+        }
     }
 }
